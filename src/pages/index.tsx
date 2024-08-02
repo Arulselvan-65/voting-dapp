3951d53cc6ab913ec3b26792bb07285960d57b10
Arulselvan <arulselvan2244@gmail.com>
2024-08-02 09:38:52 +0530

    UI updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,10 +9,10 @@ const Home: NextPage = () => {
     <div>
       <div className='flex flex-col items-center justify-center mt-20'>
         <h1 className='font-montserrat text-4xl md:text-6xl tracking-wide font-bold text-gray-800 mb-0'>Welcome to 
-          <span className="bg-gradient-to-tr from-gray-700 to-orange-600 text-transparent bg-clip-text">VotAms</span></h1>
-        <p className='text-lg text-gray-600 text-center' style={{opacity: 1}}>Blockchain-Powered Votes: Secure, Transparent, and Immutable!</p>
+          <span className="bg-gradient-to-tr from-gray-700 to-orange-600 text-transparent bg-clip-text"> VotAms</span></h1>
+        <p className='text-lg text-gray-600 text-center mt-2' style={{opacity: 1}}>Blockchain-Powered Votes: Secure, Transparent, and Immutable!</p>
         
-        <div className="my-4">
+        <div className="my-6">
           <Button onclick={null} text={"Get Started"}/>
         </div>
       
